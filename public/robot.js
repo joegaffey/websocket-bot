c36cfb4eb733d3fa4d672a0b1e732f8ef1bb86d2
Joe Gaffey <joe@joegaffey.com>
2016-09-24 22:31:56 +0100

    Taught robot how to speak english

--- public/robot.js
+++ public/robot.js
@@ -12,26 +12,32 @@ actions.reset = 8;
 var distance = 9999;
 
 robot_left = function() {
+  robot_say('Turning left');
   sendMessage(actions.left);
 };
 
 robot_right = function() {
+  robot_say('Turning right');
   sendMessage(actions.right);
 };
 
 robot_forward = function() {
+  robot_say('Advancing');
   sendMessage(actions.forward);
 };
 
 robot_backward = function() {
+  robot_say('Reversing');
   sendMessage(actions.backward);
 };
 
 robot_ledOn = function() {
+  robot_say('LED on');
   sendMessage(actions.ledOn);
 };
 
 robot_ledOff = function() {
+  robot_say('LED off');
   sendMessage(actions.ledOff);
 };
 
@@ -44,9 +50,14 @@ robot_ping = function() {
 };
 
 robot_reset = function() {
+  robot_say('Rebooting');
   sendMessage(actions.reset);
 };
 
 robot_get_distance = function() {
   return distance;
 };
+
+function robot_say(text) {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+}
