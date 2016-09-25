a2f5e16323fde6c2d4a77368cc199b889e83d4e0
Joe Gaffey <joe@joegaffey.com>
2016-09-25 09:33:08 +0100

    Moved all speech to robot interface

--- public/robot.js
+++ public/robot.js
@@ -9,7 +9,8 @@ actions.ledOff = 6;
 actions.ping = 7;
 actions.reset = 8;
 
-var distance = 9999;
+var distance = 999;
+var lastDistance = distance;
 
 robot_left = function() {
   robot_say('Turning left');
@@ -55,9 +56,17 @@ robot_reset = function() {
 };
 
 robot_get_distance = function() {
+  if(lastDistance != distance) {
+    if(distance === -1)
+      robot_say('Your algorithim is flawed. You have failed your mission.');
+    else if(distance === -2)
+      robot_say('Congratulations. Your algorithim is satisfactory. You have reached your destination.');
+  }
+  lastDistance = distance;
   return distance;
 };
 
 function robot_say(text) {
+  window.speechSynthesis.cancel();
   window.speechSynthesis.speak(new SpeechSynthesisUtterance(text));
 }
